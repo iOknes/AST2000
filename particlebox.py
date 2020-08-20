f0587c3cb383d69384de569f5e8341470d77047d
Iver Oknes <iver@oknes.no>
2020-08-20 11:28:57 +0200

    Add 3D particle box

--- particlebox.py
+++ particlebox.py
@@ -2,22 +2,19 @@ import numpy as np
 from xyz import savexyz
 
 #Particle parameters
-pN = 100
-pn = round(np.sqrt(pN))
-pN = int(pn**2)
+pN = 1000
 
 #Simulation parameters
 dt = 1e-2
 N = 1000
 l = 1
+dim = 3
 
+r_init = np.random.uniform(0, 1, (pN, dim))
+v_init = np.random.normal(0, 1, (pN, dim))
 
-rx, ry = np.meshgrid(np.arange(pn)/pn, np.arange(pn)/pn)
-r_init = np.array([rx.flatten(), ry.flatten()]).transpose()
-v_init = np.random.normal(0, 1, (pN, 2))
-
-r = np.zeros((N, pN, 2))
-v = np.zeros((N, pN, 2))
+r = np.zeros((N, pN, dim))
+v = np.zeros((N, pN, dim))
 r[0] = r_init
 v[0] = v_init
 
