d77e39b3aab223b6cfb1d3d42e0eefbc1b5ec9e9
nidorz <robin.be@hotmail.com>
2020-09-01 12:40:40 +0200

    Updated Docstring

--- modules/particle_box.py
+++ modules/particle_box.py
@@ -6,7 +6,6 @@ import faulthandler; faulthandler.enable()
 @jit(cache=True, nopython=True)
 def sim_box(pos, vel, L, nozzle, steps, dt):
     """
-    UPDATE LATER
     input:
     pos: array of position values of particles
     vel: array of velocity values of particles
@@ -14,40 +13,11 @@ def sim_box(pos, vel, L, nozzle, steps, dt):
     nozzle: side length of nozzle opening
     steps: how many steps to run the sim for
     dt: timestep
-    m: mass of particle
 
     output:
     number of escaped particles
-    accumulated momentum lost through nozzle
-
-
-    For loop advances one step, then checks whether particles are within
-    the nozzle area in the xy-plane, then checks whether the particles
-    are outside the bottom of the box in the z-dimension and within
-    the nozzle in the xy-plane.
-
-    xy_esc is a boolean array for particles that are within the nozzle
-    area in the xy-plane
-    z_esc is a boolean array for particles that are within the nozzle
-    area in the xy-plane(from xy_esc), and outside the bottom of the
-    chamber in the z-dimension
-
-    The funcion then checks which values of z_esc are True, if True it
-    adds one particle to the part_esc count, and adds that particles
-    momentum to the momentum_esc. The momentum is calculated using the
-    velocity of the particle in the z-direction, since this is the only
-    direction that we are interested in based on our nozzle location.
-
-    The function then check all particle positions, we use two boolean
-    arrays, one that checks if the particle is outside on the positive
-    side (L/2), and one for the negative side -(L/2), True if particle
-    outside. We then loop over the flattened arrays, checking the
-    boolean value, if the particle is outside(True), we reverse the
-    velocity of the particle, and correct it's position by putting it
-    back inside the chamber, corrected by how far it was outside.
-
-    The function returns the number of escaped particles, and the
-    accumulated momentum "lost" through the nozzle.
+    accumulated velocity lost through nozzle
+    accumulated velocity that hit all walls
     """
 
     part_esc = 0
