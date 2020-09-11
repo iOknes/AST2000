f20c85f21bb09ee8b3adfb7411ed50b01715b80f
nidorz <robin.be@hotmail.com>
2020-09-11 22:24:16 +0200

    Implemented Launch

--- rocket.py
+++ rocket.py
@@ -1,5 +1,5 @@
 import time
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #from numba import jit
 import multiprocessing as mp
 import faulthandler
@@ -32,7 +32,7 @@ class Rocket():
         self.rocket_mass = self.SM.spacecraft_mass
         self.planet_mass_solar = self.SS.masses[0] # in Solar Masses
         self.planet_mass_kg = self.planet_mass_solar * self.solar_mass #kg
-        self.planet_radius = self.SS.radii[0]*1e3 # radius in km
+        self.planet_radius = self.SS.radii[0]*1e3 # radius in m
         self.g = (self.G * self.planet_mass_kg) / (self.planet_radius**2)
 
 
@@ -42,11 +42,12 @@ class Rocket():
         self.rotational_period = self.SS.rotational_periods[0] # in days
         self.rotational_speed = ((self.rotational_period/(24*60*60)) *
                                   (2*np.pi*self.planet_radius)) # m/s
+        self.omega = (2*np.pi) / (24*60*60)
 
 
         #print("\n")
         #print(f"m_craft: {self.rocket_mass}")
-        print(f"ev: {self.escape_velocity:.2e}")
+        #print(f"ev: {self.escape_velocity:.2e}")
         #print(f"g: {self.g:.2f}")
         #print(f"N_craft: {self.g*self.rocket_mass:.2e}")
         #print("\n")
@@ -155,50 +156,92 @@ class Rocket():
         return g
 
     def Sim_Rocket_Launch(self, N, fuel_mass, dt):
+        # To visualise the planet
+        theta = np.linspace(0,2*np.pi, 1000)
+        x,y = self.planet_radius*np.cos(theta), self.planet_radius*np.sin(theta)
+
         thrust = self.log["F"] * N
         f_s = self.log["fuel_used"] * N
+        omega = self.omega
 
         r = np.zeros((2))
+        r[0] = self.planet_radius
         v = np.zeros((2))
+        v_omega = np.zeros((2))
         a = np.zeros((2))
 
-        # r_mag = np.sqrt(np.sum(r**2))
+        r_mag = np.sqrt(np.sum(r**2))
         v_mag = np.sqrt(np.sum(v**2))
+
+        v_omega[0] = -omega*r[1]
+        v_omega[1] = omega*r[0]
         # ang_vel v = omega*r
         # a = thrust / (self.rocket_mass + fuel_mass ) - Grav_Acc(r)
 
-        while v_mag > self.escape_velocity:
-            v_mag += np.ones((2))*100
+        t = 0
+        r_log = []
+        liftoff_check = ((thrust/(self.rocket_mass+fuel_mass))-
+                          self.Grav_Acc(r_mag))
+        if liftoff_check < 0:
+            print("Not enough thrust")
+            return
 
+        while v_mag < self.escape_velocity:
+            #r_log.append(r)
+            r_mag = np.sqrt(np.sum(r**2))
+            u_r = (r / r_mag)
 
+            a = ((thrust / (self.rocket_mass + fuel_mass)) -
+                  self.Grav_Acc(r_mag)) * u_r
 
+            v = v + (a*dt)
+            v_omega[0] = -omega*r[1]
+            v_omega[1] = omega*r[0]
+            v_tot = v + v_omega
+            v_mag = np.sqrt(np.sum(v**2))
 
+            r = r + (v_tot)*dt + (0.5 * a * (dt**2))
+            t += dt
+            fuel_mass -= f_s * dt
+            if fuel_mass < 0:
+                print("Ran out of fuel")
+                print(f"v: {v_mag:.2e}")
+                print(f"v_rem: {self.escape_velocity - v_mag:.2e}")
+                print(f"t: {t:.2e}")
+                break
 
-
-
-
-
-
-
-
-
+            if t > (60*20):
+                print("Ran out of fuel")
+                print(f"v: {v_mag:.2e}")
+                print("Took too long")
+                print(f"t: {t:.2e}")
+                break
+        """
+        r_log = (np.asarray(r_log))
+        plt.figure(1, figsize=(9,9))
+        plt.plot(x,y)
+        plt.plot(r_log[:,0], r_log[:,1], marker = "*")
+        plt.show()
+        """
+        print(f"v: {v_mag:.2e}")
+        print(f"v_rem: {self.escape_velocity-v_mag:.2e}")
+        print(f"t: {t}")
+        print(f"fuel_left: {fuel_mass}")
 
 
 if __name__ == "__main__":
-
-    #username = "ivero"
-    username = "jrevense"
+    # We're using Iver's seed from now on
+    username = "ivero"
+    #username = "jrevense"
 
     RC1 = Rocket_Chamber(username = username,
                          temp = 3e3,
                          time_run = 1e-9,
-                         dt=1e-12,
+                         dt=1e-14,
                          num_part = 1e5,
                          cache = True)
-    t_0 = time.time()
+
     RC1.run_chamber_mp()
-    t_1 = time.time()
-    #print(t_1 - t_0, "\n")
     #RC1.print_data()
 
     id = RC1.id
@@ -207,7 +250,7 @@ if __name__ == "__main__":
 
     R = Rocket(log_name = log_name, username = username)
     num_box = 5e14#6.67e14#1e14
-    fuel_mass = 2500 #kg
+    fuel_mass = 25000 #kg
     dt = 1e-3
     dv = R.escape_velocity
 
