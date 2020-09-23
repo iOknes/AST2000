4529322d8d5a8ad484cf7f4ae7f5477d52d84e89
Iver Oknes <iver@oknes.no>
2020-09-24 01:02:20 +0200

    Add part 3 with basic model for habitable zone.

---
+++ part_3/part3.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import ast2000tools.utils as utils
+import ast2000tools.constants as const
+from ast2000tools.solar_system import SolarSystem
+from ast2000tools.space_mission import SpaceMission
+
+username = "ivero"
+seed = utils.get_seed(username)
+SolSys = SolarSystem(seed)
+
+if __name__ == "__main__":
+    emittance = const.sigma * SolSys.star_temperature**4
+    target_irradiance = 1361.0
+    d = np.sqrt(const.sigma * SolSys.star_temperature**4 * (SolSys.star_radius * 1e3)**2 / target_irradiance)
+    d_AU = d / const.AU
+    print(f"Habitable zone: {d_AU:.2f}AU +/- {d_AU * 0.05:.2f}AU")
+    print((SolSys.semi_major_axes <= d_AU * 10) * (SolSys.semi_major_axes >= d_AU * 0.38))
