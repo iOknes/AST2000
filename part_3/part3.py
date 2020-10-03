3ec5d56cba93cffa633522e93b3c9e41a6606b79
Iver Oknes <iver@oknes.no>
2020-10-03 10:47:03 +0200

    Add class for simulating space mission.

--- part_3/part3.py
+++ part_3/part3.py
@@ -44,6 +44,14 @@ def find_lander_panel_size(solar_system, target_planet, efficiency=0.12, target_
     return target_effect * semi_major_axis**2 / \
     (efficiency * const.sigma * solar_system.star_temperature**4 * star_radius**2)
 
+class SpaceMission:
+    def __init__(self, launch_position, launch_time, log_dir="logs/numerical_long.npy"):
+        log_dir += ".npy" if log_dir[-4:] != ".npy" else ''
+        infile = np.load(log_dir, allow_pickle=True)
+        t = infile['times']
+        r = infile['planet_positions'].T
+        
+
 if __name__ == "__main__":
     username = "ivero"
     seed = utils.get_seed(username)
